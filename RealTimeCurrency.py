2035e331e43198520aaa4cb0304c4af89f9f295d
Krishnapriya Sahoo <162333202+KrishnapriyaSahoo@users.noreply.github.com>
2025-05-27 21:49:59 +0530

    Add files via upload

---
+++ RealTimeCurrency.py
@@ -0,0 +1,17 @@
+from forex_python.converter import CurrencyRates
+def RealTimeCurrencyExchangeRate(from_currency,to_currency,api_key):
+    import requests ,json
+    base_url=r"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE"
+    main_url=base_url+"&from_currency="+from_currency+"&to_currency="+to_currency+"&apikey="+api_key
+    req_ob=requests.get(main_url)
+    result=req_ob.json()
+    print("Result before parsing the json data:\n",result)
+    print("\nAfter parsing:\nRealtime Currency Exchange Rate for",result["Realtime Currency Exchange Rate"]
+          ["2.From_Currency Name"],"To",result["Realtime Currency Exchange Rate"]
+          ["4.To_Currency Name"],"is",result["Realtime Currency Exchange Rate"]
+          ["5.Exchange Rate"],to_currency)
+if __name__=="__main__":
+    from_currency="USD"
+    to_currency="INR"
+    api_key="Your_Api_Key"
+    RealTimeCurrencyExchangeRate(from_currency,to_currency,api_key)
