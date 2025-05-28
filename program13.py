4515b089682f7e9bf260fcf4b871ea8ac899840f
Krishnapriya Sahoo <162333202+KrishnapriyaSahoo@users.noreply.github.com>
2025-05-28 11:13:31 +0530

    Add files via upload

---
+++ program13.py
@@ -0,0 +1,9 @@
+from tkinter import *
+win=Tk()
+win.title("WsCube Tech")
+win.config(bg="yellow")
+lab=Label(win,text="krishnapriya",font=("Time New Roman",30,"bold"),bg="pink")
+lab.grid(row=0,column=0,padx=20,pady=20)
+lab=Label(win,text="sahoo",font=("Time New Roman",30,"bold"),bg="pink")
+lab.grid(row=1,column=0)
+win.mainloop()
